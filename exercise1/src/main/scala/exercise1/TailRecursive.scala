742da5affcd5599ae5353c5eb59445b51cede863
Wheaties <owreese@gmail.com>
2016-11-11 08:05:03 -0500

    more formatting

--- exercise1/src/main/scala/exercise1/TailRecursive.scala
+++ exercise1/src/main/scala/exercise1/TailRecursive.scala
@@ -6,7 +6,7 @@ object TailRecursive{
   def largest(tree: Tree[Int]): List[Int] ={
     val candidates = tree.leaves.map{ largest(_) }
     val best = candidates.foldLeft(List.empty[Int]){
-    case (Nil, right) => right
+      case (Nil, right) => right
   	  case (left, right) => if(left.sum <= right.sum) right else left
     }
     tree.value :: best
