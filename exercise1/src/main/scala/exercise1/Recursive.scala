8e10d7e9d92e80f9b1e540417b0baa28db6ce421
Wheaties <owreese@gmail.com>
2016-11-11 08:04:20 -0500

    formatting

--- exercise1/src/main/scala/exercise1/Recursive.scala
+++ exercise1/src/main/scala/exercise1/Recursive.scala
@@ -4,7 +4,7 @@ object Recursive{
   def largest(tree: Tree[Int]): List[Int] ={
 	  val candidates = tree.leaves.map{ largest(_) }
 	  val best = candidates.foldLeft(List.empty[Int]){
-      case (Nil, right) => right
+        case (Nil, right) => right
 	  	case (left, right) => if(left.sum <= right.sum) right else left
 	  }
 	  tree.value :: best
